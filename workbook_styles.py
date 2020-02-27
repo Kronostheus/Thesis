caa7bcb1d5c8c3a8db25f97befb3ad43ca50e312
Kronos <nuno.amaro@tecnico.ulisboa.pt>
2020-02-27 19:02:52 +0000

    Styling now can take into consideration more files
    Topics already classified in other documents are now taken into consideration.
    This alows each cell to have up to 4 different look-up values.
    In cases where two files have different classifications for the same topic, the highest valued one is deemed correct

--- workbook_styles.py
+++ workbook_styles.py
@@ -1,25 +1,39 @@
 import openpyxl as px
-import pandas as pd
 import glob
 
 WORKBOOK_DIR = "Data/Workbooks/"
 
 COLS = ['C', 'F']
-STYLE_VALUE = {0: 'Bad', 0.5: 'Neutral', 1: 'Good'}
+
+STYLE_VALUES = {"Good": 1, "Neutral": 0.5, "Bad": 0}
+VALUES_STYLE = {1: "Good", 0.5: "Neutral", 0: "Bad"}
 
 files = [file for file in glob.glob(WORKBOOK_DIR + '*.xlsx')]
 
-to_process = [file for file in files if px.load_workbook(file).active['C2'].style not in STYLE_VALUE.values()]
-to_match = [file for file in files if file not in to_process][-1]
+to_process = [file for file in files if px.load_workbook(file).active['C2'].style not in STYLE_VALUES.keys()]
+to_match = [file for file in files if file not in to_process]
 
-last_wb = px.load_workbook(to_match).active
+
+def decide_style(topic_dict, cell):
+    best_style = VALUES_STYLE[max(STYLE_VALUES[cell.style], STYLE_VALUES[topic_dict[cell.internal_value]])] \
+        if cell.internal_value in topic_dict.keys() else cell.style
+    return best_style
+
 
 last_wb_dict = {}
 
-for row in range(2, last_wb.max_row + 1):
-    first_topic, second_topic = 'C' + str(row), 'F' + str(row)
-    last_wb_dict[row] = {last_wb[first_topic].internal_value: last_wb[first_topic].style,
-                         last_wb[second_topic].internal_value: last_wb[second_topic].style}
+for file in to_match:
+    last_wb = px.load_workbook(file).active
+    for row in range(2, last_wb.max_row + 1):
+        first_col, second_col = 'C' + str(row), 'F' + str(row)
+        first_topic, second_topic = last_wb[first_col], last_wb[second_col]
+
+        if row in last_wb_dict.keys():
+            last_wb_dict[row][first_topic.internal_value] = decide_style(last_wb_dict[row], first_topic)
+            last_wb_dict[row][second_topic.internal_value] = decide_style(last_wb_dict[row], second_topic)
+        else:
+            last_wb_dict[row] = {first_topic.internal_value: first_topic.style,
+                                 second_topic.internal_value: second_topic.style}
 
 for file in to_process:
     wb = px.load_workbook(file)
