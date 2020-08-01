2891759a1563766a097988518a91d059fa773275
Kronos <nuno.amaro@tecnico.ulisboa.pt>
2020-08-01 11:00:09 +0100

    Update lm_data.py

--- lm_data.py
+++ lm_data.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from pathlib import Path
+from Utils import Config
 
 # data = [('train', pd.read_csv('Data/Classification/Train/train.csv')),
 #         ('test', pd.read_csv('Data/Classification/Test/test.csv')),
 #         ('val', pd.read_csv('Data/Classification/Val/val.csv'))]
 
-data = [('train_translated', pd.read_csv('Data/Classification/Train/train_translated.csv').sample(frac=1, random_state=42))]
+data = [('train_combinations', pd.read_csv(Config.TRAIN_DIR + 'train_combinations.csv').sample(frac=1, random_state=42))]
 
 Path('Data/LanguageModeling/').mkdir(exist_ok=True)
 
 for filename, df in data:
+    df = df[~df.duplicated()]
     text = df.Text.astype(str).values
     # Important: ensure UTF-8 encoding
     with open('Data/LanguageModeling/{}.txt'.format(filename), 'w', encoding='utf-8') as file:
