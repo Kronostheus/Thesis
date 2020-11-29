8af354677d236033b546f951789e236aac1869fe
Kronos <nuno.amaro@tecnico.ulisboa.pt>
2020-11-29 11:55:33 +0000

    Update lm_data.py

--- lm_data.py
+++ lm_data.py
@@ -1,25 +1,32 @@
 import pandas as pd
 from pathlib import Path
 from Utils import Config
+from tqdm import tqdm
 
-# data = [('train', pd.read_csv('Data/Classification/Train/train.csv')),
-#         ('test', pd.read_csv('Data/Classification/Test/test.csv')),
-#         ('val', pd.read_csv('Data/Classification/Val/val.csv'))]
-
-data = [('train_combinations', pd.read_csv(Config.TRAIN_DIR + 'train_combinations.csv').sample(frac=1, random_state=42))]
+data = [
+    ('train', pd.read_csv(Config.TRAIN_DIR + 'train.csv')),
+    ('train_translated', pd.read_csv(Config.TRAIN_DIR + 'train_translated.csv')),
+    ('train_combinations', pd.read_csv(Config.TRAIN_DIR + 'train_combinations.csv')),
+    ('test', pd.read_csv(Config.TEST_DIR + 'test.csv')),
+    ('val', pd.read_csv(Config.VAL_DIR + 'val.csv'))
+]
 
 Path('Data/LanguageModeling/').mkdir(exist_ok=True)
 
 for filename, df in data:
+    print("Processing: {}".format(filename))
+
     df = df[~df.duplicated()]
     text = df.Text.astype(str).values
+
     # Important: ensure UTF-8 encoding
     with open('Data/LanguageModeling/{}.txt'.format(filename), 'w', encoding='utf-8') as file:
         # Each span is a line in the text file
-        for span in text:
+        for span in tqdm(text):
             try:
                 file.write('{}\n'.format(span))
-            except:
+            except Exception as e:
+                print("Encountered Error: {}\nText: {}".format(e, span))
                 # Ignore potential errors
                 continue
 
